17df4e5d4a1cf6cfb10a6473b47003f92205ce42
Lucas Costa <costa.lima.lucas22@gmail.com>
2023-03-27 17:38:21 -0300

    add user model

---
+++ app/Models/UserModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'name',
+        'email',
+        'password',
+        'reset_hash',
+        'reset_hash_expiration_time',
+        'img',
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+
+    // Callbacks
+    protected $beforeInsert   = [];
+    protected $beforeUpdate   = [];
+}
